7a31563ea1ad2fa9a1a90031a3834890e1e99b29
Emily Whitaker <ewhitaker524@gmail.com>
2022-09-01 10:33:20 -0400

    WIP

---
+++ code/SV SOCR team .R
@@ -0,0 +1,14 @@
+# Load required packages
+library(FSA); library(dplyr);library(magrittr);library(tidyr) # data management
+library(mgcv);library(nlme); library(lme4) # modeling
+library(viridisLite); library(gridExtra); library(ggplot2) # data viz
+library(lubridate) # dealing with dates
+library(ggpubr); library(fuzzyjoin)
+library(pastecs)
+library('unikn')
+library(tidyverse)
+library(lubridate)
+library(patchwork)
+library(scales)
+
+#load in data 

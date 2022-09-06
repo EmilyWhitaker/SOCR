31281fa3ce55b2f7bcc8dd6276c245d513b6ff9c
Emily Whitaker <ewhitaker524@gmail.com>
2022-09-06 08:07:26 -0400

    WIP

--- code/TotalAllTeamMembers.R
+++ code/TotalAllTeamMembers.R
@@ -39,6 +39,39 @@ SOCR_AllTeam_Totals_Task_time <- ggplot(SOCR_work_by_Task_Type, aes(x= Date, y=A
 SOCR_AllTeam_Totals_Task_time
 ggsave("All_members_totals_TaskType.png", plot = last_plot(), height = 10, width = 12, units = "in")
 
+
+SOCR_work_by_Task_Type_Agg  <- aggregate(SOCR_work_by_Task_Type["Agreements"], by = list(SOCR_work_by_Task_Type$Date, SOCR_work_by_Task_Type$Task ), FUN = sum)
+
+
+SOCR_work_by_Task_Type_Agg<-SOCR_work_by_Task_Type_Agg %>%
+  rename( 'Date'=Group.1,
+          'Task' = Group.2,
+          "Agreements"= Agreements)
+
+
+SOCR_Totals_Task_time <- ggplot(SOCR_work_by_Task_Type_Agg, aes(x= Date, y=Agreements))+
+  geom_line()+
+  geom_point(size=4)+
+  #ylim(0,200)+
+  geom_vline(xintercept = as.numeric(as.Date("2021-12-31")), linetype=1)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-03-31")), linetype=1)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-06-30")), linetype=1)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-09-30")), linetype=1)+
+  geom_vline(xintercept = as.numeric(as.Date("2021-10-31")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2021-11-30")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-01-31")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-02-28")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-04-30")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-05-31")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-07-30")), linetype=3)+
+  geom_vline(xintercept = as.numeric(as.Date("2022-08-31")), linetype=3)+
+  theme_bw() +
+  facet_grid('Task')
+SOCR_Totals_Task_time
+
+
+
+
 ###Not by Task 
 SOSOCR_work_pp_no_task= subset(SOCR_work_pp, select = c(Date, User, Agreements))
 SOCR_work_by_Person<- rbind (SOSOCR_work_pp_no_task, SOCR_SVTeam_Totals)
